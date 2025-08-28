7c6f08e55e76dd6b3769d93eb1433f94aaedd528
Jianxin <jianxinz233@gmail.com>
2025-08-28 15:40:29 +0200

    feat(orders):add GET/POST orders with transactional stock validation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import db from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.get("/", async (req, res) => {
 
 // Mount product routes
 app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
